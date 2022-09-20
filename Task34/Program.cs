b764325dd1c90dc4390050bc294e2b371f05e5d7
Lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-09-20 22:27:24 +0400

    task34

---
+++ Task34/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] array = new int[20];
+
+void FindNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+
+        }
+    }
+    Console.WriteLine($"число четных чисел в массиве :{count} ");
+}
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(0, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{array[index]}, ");
+        index++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+FindNumber(array);
+
